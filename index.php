a367311f3eec0dac278b057d9797418ed4133963
chuista <charlotte.pacaud2006@gmail.com>
2024-12-18 11:14:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-caca
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Plushevee</title>
+        <link rel="stylesheet" href="" >
+    </head>
+
+    <body>
+        
+    </body>
+
+    <footer>
+        <p>Copyright © Charlotte Pacaud & Ines Kibach</p>
+    </footer>
